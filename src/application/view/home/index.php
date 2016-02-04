766b3e158ac277ae3677df6d659b6e689d08966f
NathanHealea <nate.healea@gmail.com>
2016-02-04 14:23:46 -0800

    Update view/home/index -- edited landing paged to described  service. Update landing-page css -- changed coloring of header text

--- src/application/view/home/index.php
+++ src/application/view/home/index.php
@@ -12,21 +12,35 @@
 
     <!-- Google fonts !-->
     <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    <!-- CSS -->
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="<?php echo URL; ?>libs/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
     <link href="<?php echo URL; ?>css/landing-page.css" rel="stylesheet">
 </head>
 <body>
 <div class="container">
     <div class="content">
-        <?php require APP . 'view/_templates/header.php'; ?>
-        <?php ##require APP . 'view/_templates/navigation.php'; ?>
-        <div>
-            <p>You are in the View: application/view/home/index.php (everything in the box comes from this file)</p>
-            <p>In a real application this could be the homepage.</p>
-        </div>
-        <div>
-            <p><strong>Forked: </strong>Panique / Mini</p>
-        </div>
+        <header class="col-lg-12">
+            <h1>WordSalad</h1>
+        </header>
+        <section class="col-lg-offset-3 col-lg-6">
+            <p>WordSalad is a text analysis service designed to analyze the authenticity of private writing samples.
+                WordSalad verifies that a writing sample was written by a student rather then fabricated using other
+                means such as text to speech or random key strokes.</p>
+        </section>
+            <footer class="col-lg-12 text-center">
+                <!-- Default to the left -->
+                <strong>Copyright &copy; 2015 <a href="http://www.oregonstate.edu/." target="_blank">Oregon State University</a>.</strong> All rights reserved.
+            </footer>
     </div>
+</div>
+<!-- jQuery 2.1.4 -->
+<script src="<?php echo URL; ?>libs/AdminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+<!-- Bootstrap 3.3.5 -->
+<script src="<?php echo URL; ?>libs/bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
